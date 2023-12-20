0e74610684b949e71dda5ebbd0810ad8fae6892b
naseemshahid <naseemshahid69@gmail.com>
2023-12-20 19:48:05 +0500

    modified array_utils.c

--- exercises/array_utils.c
+++ exercises/array_utils.c
@@ -62,28 +62,163 @@ void freeTable(int **table, int n) {
 
 double getMean(const int *arr, int size) {
   //TODO: implement
+  
+
+double avg;
+
+int sum=0;
+
+  for(int i=0;i<size;i++){
+
+sum=sum+ *arr;
+arr++;
+
+  }
+
+avg = sum/size;
+
+  return avg;
+
+
 }
 
 int getMin(const int *arr, int size) {
   //TODO: implement
-}
 
+  int min=arr[0];
+
+for(int i=1;i<size;i++){
+
+if(arr[i]<min){
+
+min=arr[i];
+}
+}
+return min;
+}
 int getIndexOfMin(const int *arr, int size) {
   //TODO: implement
+
+  int min=arr[0];
+
+for(int i=1;i<size;i++){
+
+if(arr[i]<min){
+
+min=arr[i];
+ 
+}
+
+}
+
+for(int i=0;i<size;i++){
+
+if(min==arr[i]){
+
+return i;
+
+}
+
+
+}
+
+
 }
 
 int getMax(const int *arr, int size) {
   //TODO: implement
+
+  
+int max=arr[0];
+
+for(int i=1;i<size;i++){
+
+if (max<arr[i]){
+
+max=arr[i];
+  
+}
+
+}
+
+return max;
 }
 
 int getIndexOfMax(const int *arr, int size) {
   //TODO: implement
+
+   int max=arr[0];
+
+for(int i=1;i<size;i++){
+
+if (max<arr[i]){
+
+max=arr[i];
+  
+}
 }
 
+for(int i=0;i<size;i++){
+
+if(max==arr[i]){
+
+return i;
+}
+}
+}
 int * filterThreshold(const int *arr, int size, int threshold, int *resultSize) {
   //TODO: implement
+
+  int count=0;
+
+for(int i=0;i<size;i++){
+
+if(arr[i] >= threshold){
+
+count++;
+
+}
+}
+int *dynamicarr = (int*) malloc(4*count); 
+
+*resultSize = count;
+
+int index=0;
+
+if(dynamicarr==NULL){
+
+  printf("\nMemory Allocation Error\n");
+  exit(1);
+
+}
+
+for (int i = 0; i < size; i++)
+{
+  if(arr[i] >= threshold) {
+
+dynamicarr[index]=arr[i];
+
+index++;
+
+  }
+
+}
+return dynamicarr;
 }
 
 int **createMultiplicationTable(int n, int m) {
   //TODO: implement
+
+	int **mt=(int**)malloc(n*sizeof(int*));
+	for(int i=0;i<n;i++)
+	{
+		mt[i]=(int*)malloc(m*sizeof(int));
+		for(int j=0;j<m;j++)
+		{
+			
+			mt[i][j]=(i+1) * (j+1); 
+		}
+	}
+	 return mt;
+
 }
